e4b457372d5cc18c10b746936257786b1791dda3
greatghoul <greatghoul@gmail.com>
2014-01-28 10:10:59 +0800

    通过 API 返回高亮结果
    通过 http://hilite.me/api 来返回语法高亮的结果，暂时打印在 console 中

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -9,8 +9,21 @@ function loadOptionData(name) {
     $('[name=' + name + ']').html(optionsHtml);
 }
 
+function processHighlightResult(result) {
+    console.log(result);
+}
+
+function hiliteme(evt) {
+    var params = $(this).serialize();
+    $.post('http://hilite.me/api', params, processHighlightResult);
+    return false;
+}
+
 $(function() {
     // Load data for laxers and styles 
     loadOptionData('lexer')
     loadOptionData('style')
+
+    // Event Bindings
+    $('#hiliteme').submit(hiliteme);
 });
